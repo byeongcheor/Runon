5e96abc11fe02861d3cbc22310380169587a45b0
byeongcheor <qudcjf1996@naver.com>
2024-09-25 09:25:40 +0900

    변경

---
+++ src/main/java/com/ict/finalproject/controller/testController.java
@@ -0,0 +1,24 @@
+package com.ict.finalproject.controller;
+
+import com.ict.finalproject.service.testService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class testController {
+
+    @Autowired
+    testService testservice;
+
+    @GetMapping("/test")
+    public ModelAndView test() {
+        ModelAndView mav = new ModelAndView();
+        String userid=testservice.selectAll();
+        mav.addObject("userid",userid);
+
+        mav.setViewName("test");
+        return mav;
+    }
+}
