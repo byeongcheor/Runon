b6df4ca938f109e2cbe7832dd5a19e1f8ae9c8f8
byeongcheor <qudcjf1996@naver.com>
2024-09-25 12:38:25 +0900

    12:38

---
+++ src/main/java/com/ict/finalproject/vo/testVO.java
@@ -0,0 +1,12 @@
+package com.ict.finalproject.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class testVO {
+
+}
